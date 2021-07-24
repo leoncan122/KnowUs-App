1e7616b3aaa8bb3e7542b98539d5003c7a8ab124
leoncan122 <leon.can122@gmail.com>
2021-07-24 16:52:43 +0200

    setup express server

---
+++ server/server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+require("dotenv").config({ path: "../.env" });
+
+const app = express();
+
+const corsConfig = {
+  origin: "http://localhost:3000",
+};
+
+app.use(cors(corsConfig));
+app.use(helmet());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.status(200).send("welcome to Knowus project application");
+});
+
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
