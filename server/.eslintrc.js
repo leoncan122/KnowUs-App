7de97827f4e05ea098097cc9c4d7588266bc376a
manpreet <manhspman5@gmail.com>
2021-07-29 14:09:55 +0200

    update file eslint

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -1,13 +1,13 @@
 module.exports = {
-  env: {
-    commonjs: true,
-    es2021: true,
-    node: true,
-  },
-  extends: ["airbnb-base", "prettier"],
-  plugins: ["prettier"],
-  parserOptions: {
-    ecmaVersion: 12,
-  },
-  rules: {},
+    env: {
+        commonjs: true,
+        es2021: true,
+        node: true,
+    },
+    extends: ["airbnb-base", "prettier"],
+    plugins: ["prettier"],
+    parserOptions: {
+        ecmaVersion: 12,
+    },
+    rules: {},
 };
--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ require("dotenv").config({ path: "../.env" });
 const app = express();
 
 const corsConfig = {
-  origin: "http://localhost:3000",
+    origin: "http://localhost:3000",
 };
 
 app.use(cors(corsConfig));
@@ -14,10 +14,10 @@ app.use(helmet());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.status(200).send("welcome to Knowus project application");
+    res.status(200).send("welcome to Knowus project application");
 });
 
 const { PORT } = process.env;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
