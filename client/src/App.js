9570d0966a1c273eeda290c1e0464c56fb36538a
Martin <martin@classdojo.com>
2021-08-02 14:51:56 +0200

    Remove unexisting import

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,13 @@
 import "./App.css";
 import React from "react";
-import { BrowserRouter, Route } from 'react-router-dom';
-
-import Request from "./pages/request/Request";
+import { BrowserRouter, Route } from "react-router-dom";
 
 function App() {
     return (
         <BrowserRouter>
-            <Route path="/user/questions" component={Request}/>;
+            <Route path="/user/questions" component={Request} />;
         </BrowserRouter>
-    )
-    
+    );
 }
 
 export default App;
