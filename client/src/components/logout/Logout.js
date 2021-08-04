a66b107049c7ee53565edd0bddb0829d4521e656
leoncan122 <leon.can122@gmail.com>
2021-08-04 13:19:39 +0200

    modify navigation styles

--- client/src/components/logout/Logout.js
+++ client/src/components/logout/Logout.js
@@ -1,20 +1,18 @@
 import "./Logout.css";
 import React, { useContext } from "react";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 import { userContext } from "../../context/userContext";
 
 const Logout = () => {
     const { setUserLoged } = useContext(userContext);
     return (
-        <Link to="/login">
-            <button
-                type="button"
-                className="logout-btn"
-                onClick={() => setUserLoged(null)}
-            >
-                Log out
-            </button>
-        </Link>
+        <button
+            type="button"
+            className="logout-btn"
+            onClick={() => setUserLoged(null)}
+        >
+            Log out
+        </button>
     );
 };
 
