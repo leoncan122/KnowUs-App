35c067220a2aac3ef8f305fe73455c4228f1b756
Jose Antonio Olivari <antonio.olivari@gmail.com>
2021-08-06 16:52:23 +0200

    get lastones ahora entrega resultados de manera correcta y conectado a heroku

--- server/app/controllers/user/home/getLastPublications.js
+++ server/app/controllers/user/home/getLastPublications.js
@@ -6,11 +6,12 @@ require("dotenv").config({ path: "../../../../.env.development.heroku" });
 
 const getLastPublications = (req, res) => {
     const query = `SELECT u.id sender_id, u.user_name sender_username, pq.text question_text ,pq.category,pq.is_answered,
-    p.id prof_id, p.user_name prof_username, a.id answer_id, a.text answer_text, a.hour
+    p.id prof_id, p.user_name prof_username, a.id answer_id, a.text answer_text, a.date
     FROM public_questions pq JOIN answers a ON pq.id = a.question_id
     JOIN users u ON pq.from_userid = u.id
     JOIN users p ON pq.to_userid = p.id
-    WHERE is_answered = true ORDER by a.hour DESC LIMIT 15;`;
+    WHERE is_answered = true ORDER by a.date DESC LIMIT 15;`;
+
     try {
         pool.connect((error, client, release) => {
             if (error) {
