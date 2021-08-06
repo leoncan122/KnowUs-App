7ca92ac61e766025f8758ea5adc6dca984637e67
leoncan122 <leon.can122@gmail.com>
2021-08-06 18:53:40 +0200

    home layout for min 900px

--- client/src/pages/home/units/Aside.js
+++ client/src/pages/home/units/Aside.js
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import fetchData from "../../../utils/fetchData";
 import "./aside.css";
 
 function Aside() {
+    const [data, setData] = useState(null);
+    const [error, setError] = useState("");
+    const [loading, setLoading] = useState(true);
+
+    console.log(error, loading);
+
+    useEffect(() => {
+        const url = "http://localhost:4000/home";
+        async function fetching() {
+            try {
+                setLoading(false);
+                const res = await fetchData(null, url, "GET");
+                const rawData = await res.json();
+
+                if (!res.ok) {
+                    setError(data.error);
+                }
+                setData(rawData);
+            } catch (err) {
+                setError("The comunication failed, try again later");
+            }
+        }
+        fetching();
+    }, []);
     return (
         <div className="aside">
             <div>Foto</div>
