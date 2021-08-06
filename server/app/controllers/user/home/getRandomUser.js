cbf499e890e235393dc90ba7fd578ac2df54ce9b
Jose Antonio Olivari <antonio.olivari@gmail.com>
2021-08-06 19:47:34 +0200

    me habia faltado el control de error

--- server/app/controllers/user/home/getRandomUser.js
+++ server/app/controllers/user/home/getRandomUser.js
@@ -4,6 +4,9 @@ const query = "select * from users order by random() limit 5;";
 
 const getRandomUser = (req, res) => {
     pool.query(query, (error, result) => {
+        if (error) {
+            return res.status(404).send({ message: error.message });
+        }
         res.status(201).json(result.rows);
     });
 };
