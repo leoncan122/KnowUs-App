5545bdf83f0ee01ae3edb2ce2da15ee460aa35b6
ronardj01 <ronardj01@gmail.com>
2021-08-11 10:04:46 +0200

    completado el componente AsideUserInfo

--- client/src/components/asideUserInfo/AsideUserInfo.js
+++ client/src/components/asideUserInfo/AsideUserInfo.js
@@ -1,17 +1,42 @@
-import React, { useContext } from "react";
-
+import React, { useState, useEffect, useContext } from "react";
+import cookieMonster from "../../utils/cookieMonster";
+import fetchData from "../../utils/fetchData";
 import { userContext } from "../../context/userContext";
 
 export default function AsideUserInfo() {
-    const { userLoged } = useContext(userContext);
+    const [userName, setUserName] = useState(null);
+    const [userProfession, setUserProfession] = useState(null);
+    const [userCity, setUserCity] = useState(null);
 
-    console.log(userLoged);
+    const { isLoged } = useContext(userContext);
+
+    useEffect(() => {
+        if (isLoged) {
+            const userId = cookieMonster("userId");
+            const url = `http://localhost:4000/home/user/${userId}`;
+
+            const fetching = async () => {
+                const data = await fetchData(false, url, "GET");
+
+                setUserName(data[0].user_name);
+                setUserProfession(data[0].profession);
+                setUserCity(data[0].city);
+            };
+            fetching();
+        }
+    });
 
     return (
         <div className="aside-user-info">
-            <h3>Lorenzo Bandoneon</h3>
-            <h3>Profession: Manager de ventas</h3>
-            <h3>Lives in: Alicante </h3>
+            <h3>
+                <span>{userName}</span>
+            </h3>
+            <h3>
+                Profession: <span>{userProfession}</span>
+            </h3>
+            <h3>
+                Lives in: <span>{userCity}</span>
+            </h3>
         </div>
     );
 }
