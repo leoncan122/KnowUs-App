bdf8796588f6b20b210429dadbe3165d375029b5
Jose Antonio Olivari <antonio.olivari@gmail.com>
2021-08-16 15:51:33 +0200

    cambios que hay que arreglar

--- client/src/components/collapse-cards/CollapseCards.js
+++ client/src/components/collapse-cards/CollapseCards.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import "./CollapseCards.css";
-import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import ExpandMore from "@material-ui/icons/ExpandMore";
 // import ExpandLessIcon from '@material-ui/icons/ExpandLess';
 // import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
@@ -13,13 +13,13 @@ const CollapseCards = ({ posts }) => {
                 <p>Ask: {posts.question_text}</p>
                 <p>#{posts.category}</p>
                 <div className="arrow-btn">
-                    <ExpandMoreIcon
+                    <ExpandMore
                         type="button"
                         className="toggleUp"
                         onClick={() => setIsOpen(!isOpen)}
                     >
                         Ve la respuesta
-                    </ExpandMoreIcon>
+                    </ExpandMore>
                 </div>
             </div>
 
