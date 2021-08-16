adbe75066d48bcd3e54d1eddbae53d5b82ed6c85
Jose Antonio Olivari <antonio.olivari@gmail.com>
2021-08-16 13:33:26 +0200

    other profiles funcionando a medias

--- client/src/pages/profile/OthersProfiles.js
+++ client/src/pages/profile/OthersProfiles.js
@@ -1,38 +1,5 @@
 import React from "react";
 
-const url = `http://localhost:4000/home/user/4`;
-
 export default function OthersProfiles() {
-    const userProfile = ({ match }) => {
-        const [data, setData] = useState(null);
-        const [error, setError] = useState("");
-        const { id } = useParams();
-        console.log(id);
-        useEffect(() => {
-            const url = `http://localhost:4000/home/user/${id}`;
-            async function fetching() {
-                const rawData = await fetchData(null, url, "GET");
-                if (rawData.error) {
-                    setError(rawData.error);
-                }
-                setData(rawData[0]);
-            }
-            fetching();
-        }, []);
-
-        return (
-            <div className="main-content">
-                <div className="info-profile">
-                    {data && <AsideUserInfo info={data} />}
-                    {data && <ProfileImage photo={data.photo} />}
-                    <div className="interacion-btn">
-                        <Link to={`${match.url}/question`}>public question</Link>
-                        <Link to={`/messages/user/${id}`}>direct message</Link>
-                    </div>
-                </div>
-                <div className="publications" />
-                {error && (
-                    <center>
-                        <strong>{error}</strong>
-                    </center>
-                )}
+    return <div>hello world</div>;
+}
