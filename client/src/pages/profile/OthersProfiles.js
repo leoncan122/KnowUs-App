48b9396a61923b29ad5fd76600ac9bde07c8cd74
Jose Antonio Olivari <antonio.olivari@gmail.com>
2021-08-18 18:49:46 +0200

    todo funcionando sin pagina others profiles

--- client/src/pages/profile/OthersProfiles.js
+++ client/src/pages/profile/OthersProfiles.js
@@ -1,39 +1,8 @@
 import React from "react";
+// import AsideUserInfo from "../../components/asideUserInfo/AsideUserInfo";
+// import ProfileImage from "../../components/profile-image/ProfileImage";
+// import fetchData from "../../utils/fetchData";
 
 export default function OthersProfiles() {
-    const userProfile = ({ match }) => {
-        const [data, setData] = useState(null);
-        const [error, setError] = useState("");
-        const { id } = useParams();
-        console.log(id);
-        useEffect(() => {
-            const url = `http://localhost:4000/home/user/${id}`;
-            async function fetching() {
-                const rawData = await fetchData(null, url, "GET");
-                if (rawData.error) {
-                    setError(rawData.error);
-                }
-                setData(rawData[0]);
-            }
-            fetching();
-        }, []);
-    };
-    return (
-        <div className="main-content">
-            <div className="info-profile">
-                {data && <AsideUserInfo info={data} />}
-                {data && <ProfileImage photo={data.photo} />}
-                <div className="interacion-btn">
-                    <Link to={`${match.url}/question`}>public question</Link>
-                    <Link to={`/messages/user/${id}`}>direct message</Link>
-                </div>
-            </div>
-            <div className="publications" />
-            {error && (
-                <center>
-                    <strong>{error}</strong>
-                </center>
-            )}
-        </div>
-    );
+    return <div> hola </div>;
 }
