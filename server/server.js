6730949fd296bcb60d4b5f7358574668bd89512b
Jose Antonio Olivari <antonio.olivari@gmail.com>
2021-08-23 13:54:29 +0200

    cambiando cors intento 2

--- server/server.js
+++ server/server.js
@@ -43,7 +43,7 @@ const { getMessages } = require("./app/middlewares/directMessages/getMessages");
 console.log(process.env.FRONT_ORIGIN);
 
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", process.env.FRONT_ORIGIN);
+    res.header("Access-Control-Allow-Origin", process.env.FRONT_ORIGIN, "https://client-knowus.herokuapp.com");
     res.header(
         "Access-Control-Allow-Headers",
         "x-access-token, Origin, Content-Type, Accept"
