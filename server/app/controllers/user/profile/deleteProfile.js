01597a426465803c9d9f71b791dadcee12e14203
ronardj01 <ronardj01@gmail.com>
2021-08-25 08:43:11 +0200

    agregado endpoint profile/delete para eliminar user

---
+++ server/app/controllers/user/profile/deleteProfile.js
@@ -0,0 +1,64 @@
+const { pool } = require("../../../services/poolService");
+
+const query1 = `delete from answers where id in (select a.id from answers a
+    join public_questions pq on question_id = pq.id
+    join users u on pq.from_userid = u.id
+    where u.id = $1)`;
+
+const query2 = `delete from answers where id in (select a.id from answers a
+        join public_questions pq on question_id = pq.id
+        join users u on pq.to_userid = u.id
+        where u.id = $1)`;
+
+const query3 = `delete from public_questions where id in (select pq.id from public_questions pq
+    join users u on pq.from_userid = u.id
+    where u.id = $1)`;
+
+const query4 = `delete from public_questions where id in (select pq.id from public_questions pq
+    join users u on pq.to_userid = u.id
+    where u.id = $1)`;
+
+const query5 = `delete from direct_messages  where id in (select dm.id from direct_messages dm
+    join users u on dm.to_userid = u.id
+    where u.id = $1)`;
+
+const query6 = `delete from direct_messages  where id in (select dm.id from direct_messages dm
+    join users u on dm.from_userid = u.id
+    where u.id = $1)`;
+
+const query7 = `delete from users WHERE id = $1`;
+
+const queries = [query1, query2, query3, query4, query5, query6, query7];
+
+const deleteProfile = (req, res) => {
+    const userId = req.id;
+
+    try {
+        queries.forEach((query) => {
+            pool.query(query, [userId]);
+        });
+        return res.send("User Deleted");
+    } catch (error) {
+        console.log(error);
+    }
+    /* const deleteData = async () => {
+        try {
+            const client = pool.connect();
+            let result = client.query(query1, [userId]);
+            result = client.query(query2, [userId]);
+            result = client.query(query3, [userId]);
+            result = client.query(query4, [userId]);
+            result = client.query(query5, [userId]);
+            result = client.query(query6, [userId]);
+            result = client.query(query7, [userId]);
+            client.realese();
+            return res.send("User Deleted");
+        } catch (error) {
+            console.log(error);
+            client.realese();
+        }
+    };
+    deleteData(); */
+};
+
+module.exports = deleteProfile;
--- server/app/routes/profile.js
+++ server/app/routes/profile.js
@@ -2,10 +2,12 @@ const express = require("express");
 const { verifyToken } = require("../middlewares/verifyToken");
 const editProfile = require("../controllers/user/profile/editProfile");
 const getMyAnswer = require("../controllers/user/profile/getMyAnswer");
+const deleteProfile = require("../controllers/user/profile/deleteProfile");
 
 const router = express.Router();
 
 router.put("/edit", [verifyToken], editProfile);
 router.get("/cards/:userId", getMyAnswer);
+router.delete("/delete", [verifyToken], deleteProfile);
 
 module.exports = router;
